590b6bfd0fab7004b940b1d180db7b72b49b6db6
Vladislav Gerasimov <gerasimovvs@yahoo.com>
2020-09-26 02:41:53 +0200

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
-(defproject code-extractor "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
-            :url  "https://www.eclipse.org/legal/epl-2.0/"}
+(defproject tangler "1.0.0"
+  :description "Code block extractor"
+  :url "http://github.com/wlad031/tagler"
+  :license {:name "DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE Version 2"
+            :url  "http://www.wtfpl.net/"}
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [org.clojure/tools.cli "1.0.194"]
                  [com.taoensso/timbre "4.10.0"]]
